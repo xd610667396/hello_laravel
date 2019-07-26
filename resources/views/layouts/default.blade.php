7e698164557d6b20dceacc52edb3190400e74eca
vagrant <vagrant@homestead>
2019-07-26 07:53:01 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,17 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-    </div>
-</nav>
+@include('layouts._header')
 
 <div class="container">
-    @yield('content')
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 </body>
 </html>
