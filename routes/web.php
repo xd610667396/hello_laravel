ab87acce1ffcb498daf39264d7cfd867da4512f9
vagrant <vagrant@homestead>
2019-07-31 02:29:22 +0000

    提交注册功能页面

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
